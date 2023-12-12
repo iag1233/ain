48aaacdb19adaacf32a53f84c52949e0a090bf3e
iaramendi0 <144702548+iaramendi0@users.noreply.github.com>
2023-12-12 14:55:44 +0000

    aa

--- public/js/main.js
+++ public/js/main.js
@@ -4,3 +4,46 @@ const audioBlob = new Blob(audioChunks, { type: 'audio/wav' });
 const audioUrl = URL.createObjectURL(audioBlob);
 const audioPlayer = document.getElementById('audioPlayer');
 audioPlayer.src = audioUrl;*/
+
+
+class App {
+    
+    constructor() {
+      this.audio = null;
+      this.blob = null;
+      this.state = {};
+    }
+  
+    init() {
+    }
+  
+    initAudio() {
+    }
+  
+    loadBlob() {
+    }
+  
+    initRecord() {
+    }
+  
+    record() {
+    }
+  
+    stopRecording() {
+    }
+  
+    playAudio() {
+    }
+  
+    stopAudio() {
+    }
+  
+    upload() {
+    }
+  
+    deleteFile() {
+    }
+  }
+  
+  const app = new Sortu();
+  app.init();
