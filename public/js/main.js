2b60415fe4fe7628b986e1fc13b31906ebf647b7
iaramendi0 <iaramendi009@ikasle.ehu.eus>
2023-12-12 15:23:20 +0100

    aa

---
+++ public/js/main.js
@@ -0,0 +1,6 @@
+/**const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+
+const audioBlob = new Blob(audioChunks, { type: 'audio/wav' });
+const audioUrl = URL.createObjectURL(audioBlob);
+const audioPlayer = document.getElementById('audioPlayer');
+audioPlayer.src = audioUrl;*/
