e279b5d5849fe1cef263dfd9a2f0ce039dc160bf
mikelmujika16 <mmugica053@ikasle.ehu.eus>
2023-12-12 18:16:11 +0100

    tyhhdfghd

--- public/js/main.js
+++ public/js/main.js
@@ -52,10 +52,44 @@ class App {
         };
     }
   
-    loadBlob() {
+    loadBlob(blob) {
+        if (this.audio) {
+            // Set the src attribute of the audio element
+            this.audio.src = URL.createObjectURL(blob);
+        } else {
+            console.error('Audio element is not initialized.');
+        }
     }
+    
   
     initRecord(stream) {
+        // Check if MediaRecorder is supported
+    if (typeof MediaRecorder === 'undefined') {
+        console.error('MediaRecorder is not supported on this browser.');
+        return;
+    }
+
+    // Create a new MediaRecorder with the provided stream
+    this.mediaRecorder = new MediaRecorder(stream);
+
+    // Initialize audioChunks array
+    this.audioChunks = [];
+
+    // Handle ondataavailable event
+    this.mediaRecorder.ondataavailable = (event) => {
+        if (event.data.size > 0) {
+            this.audioChunks.push(event.data);
+        }
+    };
+
+    // Handle onstop event
+    this.mediaRecorder.onstop = () => {
+        // Create a Blob from the recorded audio chunks
+        this.blob = new Blob(this.audioChunks, { type: 'audio/wav' });
+
+        // Load the Blob using the loadBlob method
+        this.loadBlob(this.blob);
+    };
     }
   
     record() {
--- public/record.js
+++ public/record.js
@@ -9,6 +9,7 @@ let isRecording = false;
 async function toggleRecording() {
   if (!isRecording) {
     const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    
     mediaRecorder = new MediaRecorder(stream);
     mediaRecorder.ondataavailable = (event) => {
       audioChunks.push(event.data);
