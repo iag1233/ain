ace5b871b3d0e31c898636d175a290879084d4bb
mikelmujika16 <mmugica053@ikasle.ehu.eus>
2023-12-13 17:17:33 +0100

    3.1

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
 import { recordFn } from "/js/recordButton.js";
 import { playFn} from "/js/playButton.js";
 import { uploadFn } from "/js/uploadButton.js";
-
+import { v4 as uuidv4 } from '/utils/uuid/v4.js';
 
 class App {
 
@@ -11,6 +11,10 @@ class App {
       this.state = {};
       this.isRecording = false;
       this.playingIntervalId = null;
+      if (!localStorage.getItem("uuid")) { // localStoragen gordeta EZ badugu
+        localStorage.setItem("uuid", uuidv4()); // uuid-a sortu eta gorde
+        this.uuid = localStorage.getItem("uuid"); // localStoragetik uuid-a lortu
+      }
     }
   
     toggleRecording() {
@@ -54,30 +58,40 @@ class App {
   
     loadBlob(blob) {
         if (this.audio) {
+            // Set the src attribute of the audio element
             this.audio.src = URL.createObjectURL(blob);
         } else {
-            console.error('Audio elementua ez dago sorturik');
+            console.error('Audio element is not initialized.');
         }
     }
     
   
     initRecord(stream) {
+        // Check if MediaRecorder is supported
     if (typeof MediaRecorder === 'undefined') {
-        console.error('MediaRecorder ez da onartzen');
+        console.error('MediaRecorder is not supported on this browser.');
         return;
     }
+
+    // Create a new MediaRecorder with the provided stream
     this.mediaRecorder = new MediaRecorder(stream);
+
+    // Initialize audioChunks array
     this.audioChunks = [];
 
-
+    // Handle ondataavailable event
     this.mediaRecorder.ondataavailable = (event) => {
         if (event.data.size > 0) {
             this.audioChunks.push(event.data);
         }
     };
 
+    // Handle onstop event
     this.mediaRecorder.onstop = () => {
+        // Create a Blob from the recorded audio chunks
         this.blob = new Blob(this.audioChunks, { type: 'audio/wav' });
+
+        // Load the Blob using the loadBlob method
         this.loadBlob(this.blob);
     };
     }
@@ -88,22 +102,31 @@ class App {
     }
 
     render() {
+        // Handle UI updates based on the current state
         switch (this.state.status) {
             case 'playing':
                 this.renderPlayingState();
                 break;
+            // Add other cases for different states as needed
+
             default:
+                // Default rendering behavior
                 break;
         }
     }
 
     renderPlayingState() {
+        // Update UI for playing state, e.g., show current playback time
         const formattedTime = this.formatTime(this.audio.currentTime);
-        const remainingTime = this.formatTime(this.audio.duration - this.audio.currentTime);
-        console.log(`Current playback time: ${formattedTime}, Remaining time: ${remainingTime}`);
+    const remainingTime = this.formatTime(this.audio.duration - this.audio.currentTime);
+    
+    console.log(`Current playback time: ${formattedTime}, Remaining time: ${remainingTime}`);
+    
+    
     }
 
     formatTime(time) {
+        // Helper function to format time (e.g., from seconds to MM:SS)
         const minutes = Math.floor(time / 60);
         const seconds = Math.floor(time % 60);
         const formattedTime = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
@@ -111,8 +134,11 @@ class App {
     }
 
     record() {
+        // Check if recording is not in progress
         if (!this.isRecording) {
+            // Clear the existing audio chunks
             this.audioChunks = [];
+    
             this.mediaRecorder.start();
             this.isRecording = true;
             this.setState({ status: 'recording' });
@@ -121,6 +147,7 @@ class App {
     
     
     stopRecording() {
+        // Check if recording is in progress
         if (this.isRecording) {
             this.mediaRecorder.stop();
             this.isRecording = false;
@@ -129,25 +156,36 @@ class App {
     }
     
     playAudio() {
+        // Check if audio is available
         if (this.audio && this.blob) {
             this.audio.play();
             this.setState({ status: 'playing' });
+    
+            // Start the interval to update the UI with the remaining time
             this.playingIntervalId = setInterval(() => {
                 const remainingTime = this.formatTime(this.audio.duration - this.audio.currentTime);
+    
+                // Update the UI as needed, e.g., display remaining time next to the button
                 const liPlayButton = document.getElementById("liPlayButton");
                 liPlayButton.innerHTML = `${playFn()} ${remainingTime}`;
+    
+                // Check if the audio has finished playing
                 if (this.audio.currentTime >= this.audio.duration) {
                     clearInterval(this.playingIntervalId);
                     this.setState({ status: 'recorded' });
                 }
-            }, 1000);
+            }, 1000); // Update every second
         }
     }
     
+    // Update the stopAudio method
     stopAudio() {
+        // Check if audio is playing
         if (this.audio && !this.audio.paused) {
             this.audio.pause();
             this.audio.currentTime = 0;
+    
+            // Clear the interval when stopping audio
             clearInterval(this.playingIntervalId);
     
             this.setState({ status: 'recorded' });
@@ -155,11 +193,32 @@ class App {
     }
     
     upload() {
-        console.log('File uploaded.');
-        this.setState({ status: 'uploaded' });
+        this.setState({ uploading: true }); // uneko egoera: uploading
+        const body = new FormData(); // FormData bidez audioa zerbitzarira igo ahalko dugu
+        body.append("recording", this.blob); // formData-n recording izeneko atributuan gure audioa gorde igotzeko
+        fetch("/api/upload/" + this.uuid, {
+            method: "POST", // audioa igotzeko POST metodoa erabiliko dugu
+            body,
+        })
+        .then((res) => res.json()) // zerbitzariak audioa jaso ondoren erabiltzaile honek dituen fitxategi guztien zerrenda itzuliko du (bertan igo berri dena barne)
+        .then((json) => {
+            this.setState({
+                files: json.files, // erabiltzailearen fitxategi guztiak
+                uploading: false, // uneko egoera eguneratu
+                uploaded: true, // uneko egoera eguneratu
+                error: false, 
+                loading: false,
+            });
+        })
+        .catch((err) => {
+            console.error('Error during upload:', err);
+            this.setState({ error: true });
+        });
     }
     
     deleteFile() {
+        // Implement delete functionality based on your requirements
+        // For now, log a message to the console
         console.log('File deleted.');
         this.setState({ status: 'deleted' });
     }
@@ -169,12 +228,15 @@ class App {
   app.init();
 
 
+
+  
   window.onload = function () {
     const liRecordButton = document.getElementById("liRecordButton");
     liRecordButton.innerHTML = recordFn(app.isRecording);
 
     liRecordButton.addEventListener('click', () => {
         app.toggleRecording();
+        // Update the innerHTML of the Record button based on the recording state
         liRecordButton.innerHTML = recordFn(app.isRecording);
     });
 
